b537f923c84d60cb550beb07b3036af5e32daf66
Haibara <2586225191@qq.com>
2023-02-26 15:03:53 +0800

    have got hunters id

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import requests
 import re
-
+import xlwings as xw
+import csv
 # 伪装
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 '
@@ -13,18 +14,34 @@ header_find_job = {
                   'Safari/537.36 Edg/110.0.1587.56 '
 
 }
-Max_page = 158  # 网站最大页码
+Max_page = 2  # 网站最大页码
+
+
+# 数据导出
+def export_excel():
+    f = open('文件名.csv', 'w', encoding='utf-8')
 
 
 # 获取招聘工作id
 def get_jobid(str_mes):
     id_cnt = 0
     for j in range(10):  # 一行十个工作
-        index_id = str_mes.index('"jobPositionId"')  # 查找id项
-        if index_id:
+        index_id = str_mes.index('"jobPositionId"')  # 查找jobPositionId项
+        if index_id >= 0:
             id_cnt += 1
             print("第{}个招聘信息id：{}".format(id_cnt, str_mes[index_id + 1]))
         str_mes = str_mes[index_id + 50:]  # 跳过本条
+
+
+# 提取求职者id
+def get_hunters_id(str_mes):
+    hunterid_cnt = 0
+    for k in range(10): # 一页十条
+        hunter_id_index = str_mes.index('"username"')  # 查找id项
+        if hunter_id_index >= 0: # 可靠
+            hunterid_cnt += 1
+            print("第{}个求职者id：{}".format(hunterid_cnt, str_mes[hunter_id_index - 1]))
+        str_mes = str_mes[hunter_id_index + 10:]  # 跳过本条
 
 
 # 爬取全部数据
@@ -46,5 +63,9 @@ for i in range(1, Max_page):  # 翻页
     mes_hunter = re.split(',|:', response_hunter.text)  # 分割数据
     print("求职者第" + str(i) + "页信息:")
     print(mes_hunter)
-    print("招聘工作第" + str(i) + "页信息:")
-    print(mes_job)
+    get_hunters_id(mes_hunter)
+    # print("招聘工作第" + str(i) + "页信息:")
+    # print(mes_job)
+
+    #if __name__ == '__main__':
+
