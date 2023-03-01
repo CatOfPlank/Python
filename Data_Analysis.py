ab2c4649d1e5ae61c3b8ef9a2618496287b14b6c
Haibara <2586225191@qq.com>
2023-03-01 17:28:59 +0800

    Conversing datas to dict type

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,8 +1,12 @@
+import openpyxl
 import pandas as pd
 import requests
 import re
+import json
+import pprint
 import xlwings as xw
 import csv
+import os
 
 # 伪装
 header = {
@@ -32,7 +36,8 @@ Max_page = 2  # 网站最大页码
 
 # 数据导出
 def export_data(mes):
-    f = open('文件名.csv', 'w', encoding='utf-8')
+    json_data = json.loads(mes)  # 转成dict格式
+    pprint.pprint(json_data)  # 格式化输出
 
 
 # 获取招聘工作id
@@ -58,12 +63,13 @@ def get_job_details(job_id_list):
         job_detail_url = "https://www.5iai.com/api/enterprise/job/public?id=" + "".join(
             list(filter(str.isdigit, job_id_list[index_list])))
         detail_job_response = requests.get(url=job_detail_url, headers=header_job_detail)  # 获取详细信息
-        print("第{}个招聘工作详细要求：{}".format(index_list + 1, detail_job_response.text))
+        print("第{}个招聘工作详细要求".format(index_list + 1))
+        export_data(detail_job_response.text)
 
 
 # 提取求职者id
 def get_hunters_id(str_mes):
-    id_list = []    # 存储id的列表
+    id_list = []  # 存储id的列表
     hunter_cnt = 0
     for k in range(10):  # 一页十条
         hunter_id_index = str_mes.index('"username"')  # 查找id项
@@ -74,39 +80,43 @@ def get_hunters_id(str_mes):
         str_mes = str_mes[hunter_id_index + 10:]  # 跳过本条
     return id_list  # 返回id列表
 
+
 # 获取求职者具体简历
 def get_hunter_detail(hunter_id_list):
     for index_list in range(0, 10):
         hunter_detail_url = "https://www.5iai.com/api/resume/baseInfo/public/" + "".join(
             list(filter(str.isdigit, hunter_id_list[index_list])))
         detail_hunter_response = requests.get(url=hunter_detail_url, headers=header_hunter_detail)  # 获取求职者简历
-        print("第{}个求职者简历：{}".format(index_list + 1, detail_hunter_response.text))
+        print("第{}个求职者简历：".format(index_list + 1))
+        export_data(detail_hunter_response.text)
 
 
 # 爬取全部数据
-for i in range(1, Max_page):  # 翻页
-    url_job = "https://www.5iai.com/api/enterprise/job/public/es?pageSize=10&pageNumber=" + str(i) + "&willNature" \
-                                                                                                     "=&function" \
-                                                                                                     "=&wageList=%255B%255D" \
-                                                                                                     "&workplace" \
-                                                                                                     "=&keyword= "  # 招聘信息url
+def get_all_message():
+    for i in range(1, Max_page):  # 翻页
+        url_job = "https://www.5iai.com/api/enterprise/job/public/es?pageSize=10&pageNumber=" + str(i) + "&willNature" \
+                                                                                                         "=&function" \
+                                                                                                         "=&wageList=%255B%255D" \
+                                                                                                         "&workplace" \
+                                                                                                         "=&keyword= "  # 招聘信息url
 
-    url_find_job = "https://www.5iai.com/api/resume/baseInfo/public/es?pageSize=10&pageNumber=" + str(i) + "&function" \
-                                                                                                           "=&skills" \
-                                                                                                           "=&workplace" \
-                                                                                                           "=&keyword= "  # 求职者信息url
+        url_find_job = "https://www.5iai.com/api/resume/baseInfo/public/es?pageSize=10&pageNumber=" + str(
+            i) + "&function" \
+                 "=&skills" \
+                 "=&workplace" \
+                 "=&keyword= "  # 求职者信息url
 
-    response_job = requests.get(url=url_job, headers=header)
-    response_hunter = requests.get(url=url_find_job, headers=header_find_job)
-    mes_job = re.split(',|:', response_job.text)
-    mes_hunter = re.split(',|:', response_hunter.text)  # 分割数据
-    # print("求职者第" + str(i) + "页信息:")
-    # print(mes_hunter)
-    # get_hunters_id(mes_hunter)  # 求职者id
-    print("招聘工作第" + str(i) + "页信息:")
-    print(mes_job)
-    get_job_details(get_jobid(mes_job))
-    print("求职者第" + str(i) + "页信息:")
-    print(mes_hunter)
-    get_hunter_detail(get_hunters_id(mes_hunter))
-# if __name__ == '__main__':
+        response_job = requests.get(url=url_job, headers=header)
+        response_hunter = requests.get(url=url_find_job, headers=header_find_job)
+        mes_job = re.split(',|:', response_job.text)
+        mes_hunter = re.split(',|:', response_hunter.text)  # 分割数据
+        print("招聘工作第" + str(i) + "页信息:")
+        print(mes_job)
+        get_job_details(get_jobid(mes_job))
+        print("求职者第" + str(i) + "页信息:")
+        print(mes_hunter)
+        get_hunter_detail(get_hunters_id(mes_hunter))
+
+
+if __name__ == '__main__':
+    get_all_message()
