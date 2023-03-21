3bd1370ea9fd58b050dbc748d0ca855d081a204a
Haibara <2586225191@qq.com>
2023-03-21 22:39:16 +0800

    Adding WIFI medium.

--- WiFi.py
+++ WiFi.py
@@ -5,7 +5,7 @@ Wifi数据传输---服务端
 import sys
 import socket
 import time
-
+import  re
 import matplotlib.pyplot as plt
 import threading
 import struct
@@ -25,27 +25,34 @@ tcp_server_socket.bind(address)
 tcp_server_socket.listen(5)
 
 client_socket, clientAddr = tcp_server_socket.accept()
-
 print("等待客户端连接...")
-
+if clientAddr is not None:
+    print("连接成功！")
 
 # 接收数据
+data_buff = []
+
+
 def Receive():
+    data = client_socket.recv(3)    # 收到一个字节
+
+    data = str(data)
+    f_data = data.replace("\\" , "").replace("r","").replace("n","")
+    f_data = float(f_data)
+    data_buff.append(f_data)
     # 接收到的信息，一个字节
-    data = client_socket.recv(1024)
-    print(data.decode("gbk"))
+    if len(data_buff) >= 5:  # 收到足够的数据后绘图
+        draw(data_buff)
+        data_buff.clear()  # 清空
 
 
 # 画曲线
-def draw():
+def draw(data):
     freq = np.linspace(0, 200, 200)  # 频率轴
-    amp = []  # 幅度
-    if len(amp) >= 200:
-        plt.plot(freq, amp)
+    plt.plot(freq, data)
 
 
 if __name__ == '__main__':
     while True:
         Receive()
-        time.sleep(100)
-        draw()
+        time.sleep(0.2)
