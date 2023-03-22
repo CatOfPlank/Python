297893ca887f9791ae903b177a5b5b16e49f186f
Haibara <2586225191@qq.com>
2023-03-22 09:41:26 +0800

    Completed drawing graph.

--- WiFi.py
+++ WiFi.py
@@ -2,7 +2,7 @@
 Wifi数据传输---服务端
 
 """
-# coding = <gbk> # 支持中文
+# -*- coding: utf-8 -*-     # 支持中文
 import sys
 import socket
 import time
@@ -11,8 +11,7 @@ import matplotlib.pyplot as plt
 import threading
 import struct
 import numpy as np
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
+from mpldatacursor import datacursor
 
 # socket
 # 地址和端口号
@@ -32,18 +31,23 @@ if clientAddr is not None:
 
 # 接收数据
 data_buff = []
+amp_num = 5     # 纵坐标数据量
 
 
 def Receive():
-    data = client_socket.recv(5)  # 收到一个字节
+    data = client_socket.recv(5)  # 收到5个字节
     data = data.decode('gbk')
     data = str(data)
-    f_data = data.replace("\\", "").replace("r", "").replace("n", "")
+    f_data = data.replace("\\", "").replace("r", "").replace("n", "")   # 去掉回车换行
     f_data = float(f_data)
     data_buff.append(f_data)
+    data_buff_normalize = []    # 归一化幅度
     # 接收到的信息，一个字节
-    if len(data_buff) >= 5:  # 收到足够的数据后绘图
-        draw(data_buff)
+    if len(data_buff) >= amp_num:  # 收到足够的数据后绘图
+        max_value = max(data_buff)  # 求列表最大值
+        for i in range(0, amp_num):
+            data_buff_normalize.append(20*np.log(data_buff[i]/max_value))
+        draw(data_buff_normalize)
         data_buff.clear()  # 清空
 
 
@@ -52,8 +56,10 @@ def draw(data):
     freq = np.linspace(0, 200, 5)  # 频率轴
     plt.xlabel("Frequency/KHz")
     plt.ylabel("Normalization/dB")
-    plt.plot(freq, data)
+    plt.plot(freq, data, color='red')
     plt.show()
+    datacursor(hover=True, xytext=(0, 20),
+               formatter='{label}: {height}'.format)    # 与鼠标交互
 
 
 if __name__ == '__main__':
