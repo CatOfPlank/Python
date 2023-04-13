a569dbbd3cf60f4c92fdc372bccf51507c62a876
Haibara <2586225191@qq.com>
2023-04-13 23:56:50 +0800

    Draw wordcloud.

--- datalabel.py
+++ datalabel.py
@@ -8,6 +8,9 @@ import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+import string
+import re
 
 
 # 加载数据
@@ -22,13 +25,14 @@ def cut_words(file, output_file):
     with open(file, "r", encoding="utf-8") as data_f:
         reader = csv.reader(data_f)
         for row in reader:
-            key_words = jieba.cut(row[10])
+            jieba.load_userdict('dic.txt')  # 自定义词库
+        key_words = jieba.cut(row[21])  # 求职者为21，职位10
             # for column in columns:
             #     key_words = jieba.cut(column, cut_all=False)  # 对每一列分词
             for key_word in key_words:
                 key_word_arr.append(key_word)
     #     将分词结果保存到CSV文件中
-    with open(output_file, 'a', newline='', encoding='utf-8') as save_f:
+    with open(output_file, 'a', encoding='utf-8') as save_f:
         writer = csv.writer(save_f)
         writer.writerow(key_word_arr)
 
@@ -38,17 +42,25 @@ def create_wordcloud(file):
     with open(file, 'r', encoding='utf-8') as f:
         text = f.read()
         text = text.split(',')
-        #   计算词频
-        word_counts = Counter(text)
+        #        text = re.sub(r'[^\u4e00-\u9fa5a-zA-Z0-9]+', ',', text)  # 正则
         # 设置词云图参数
-
         wc = WordCloud(
             font_path='simhei.ttf',
             width=800,
             height=600,
             max_words=200,
-            background_color='white'
+            background_color='white',
+            relative_scaling=0.4,
+            max_font_size=160,
         )
+
+        #   计算词频
+        word_counts = Counter(text)
+        delete_words = ['"', '"', '/', '”', '“']
+        # 删除指定词汇
+        for delete_word in delete_words:
+            if delete_word in word_counts:
+                del word_counts[delete_word]
         # 生成词云图
         wc.generate_from_frequencies(word_counts)
         # 显示词云图
@@ -59,4 +71,6 @@ def create_wordcloud(file):
 
 
 if __name__ == '__main__':
-    create_wordcloud('job_key_works.txt')
+    # cut_words('result1-1.csv', 'job_key_works.txt')
+    cut_words('result1-2.csv', 'hunter_key_works.txt')
+    create_wordcloud('hunter_key_works.txt')
