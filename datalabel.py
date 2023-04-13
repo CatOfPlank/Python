96f0f74da85d90cc3385d93f6897a639c5192d06
Haibara <2586225191@qq.com>
2023-04-13 20:45:30 +0800

    Draw wordcloud.

--- datalabel.py
+++ datalabel.py
@@ -5,6 +5,9 @@
 import csv
 import jieba
 import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 # 加载数据
@@ -15,6 +18,7 @@ import pandas as pd
 # #   定义分词函数,对csv文件分词
 def cut_words(file, output_file):
     i = 0
+    key_word_arr = []
     with open(file, "r", encoding="utf-8") as data_f:
         reader = csv.reader(data_f)
         for row in reader:
@@ -22,15 +26,37 @@ def cut_words(file, output_file):
             # for column in columns:
             #     key_words = jieba.cut(column, cut_all=False)  # 对每一列分词
             for key_word in key_words:
-                print(key_word)
-                i = i + 1
-                #     将分词结果保存到CSV文件中
-                with open(output_file, 'a', newline='', encoding='utf-8') as save_f:
-                    writer = csv.writer(save_f)
-                    writer.writerows(key_word)
-    print('共有{}条信息'.format(i))
+                key_word_arr.append(key_word)
+    #     将分词结果保存到CSV文件中
+    with open(output_file, 'a', newline='', encoding='utf-8') as save_f:
+        writer = csv.writer(save_f)
+        writer.writerow(key_word_arr)
+
+
+# 生成词云图
+def create_wordcloud(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        text = text.split(',')
+        #   计算词频
+        word_counts = Counter(text)
+        # 设置词云图参数
+
+        wc = WordCloud(
+            font_path='simhei.ttf',
+            width=800,
+            height=600,
+            max_words=200,
+            background_color='white'
+        )
+        # 生成词云图
+        wc.generate_from_frequencies(word_counts)
+        # 显示词云图
+        plt.figure(figsize=(10, 8))
+        plt.imshow(wc)
+        plt.axis('off')
+        plt.show()
 
 
 if __name__ == '__main__':
-    cut_words('result1-1.csv', 'job_key_works.csv')
-    print("分词成功！")
+    create_wordcloud('job_key_works.txt')
